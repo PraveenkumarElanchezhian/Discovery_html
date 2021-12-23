a82199e579724b66c4b3c78b891969a22dee0bc9
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-12-23 23:19:54 +0530

    Gallery html page

--- Js/footer.js
+++ Js/footer.js
@@ -1,1 +1,29 @@
-document.getElementById("footer").innerHTML =''
+document.getElementById("footer").innerHTML =`<div class="footer-content">
+<div class="left">
+    <div class="image_Style"><img src="../assets/Images/footerlogo.JPG" alt="icon">
+    </div>
+    <div class="text_style">© Discovery Land Company. All rights reserved.</div>
+</div>
+<div class="right">
+     <div>
+        <div class="Inner-content">GET IN TOUCH</div>
+        <div class="Innercontent">(480) 624-5200</div>
+        <div><a class="Innercontent" href="https://mail@discoverylandco.com/">mail@discoverylandco.com</a></div>
+    </div>
+    <div>
+        <div class="Innercontent">About DLC</div>
+        <div class="Innercontent">Our Worlds</div>
+        <div class="Innercontent">Experiences</div>
+        <div class="Innercontent">Gallery</div>
+        <div class="Innercontent">Press</div>
+    </div>
+    <div>
+        <div class="Innercontent">Magazine</div>
+        <div class="Innercontent">Careers</div>
+        <div class="Innercontent">Foundation</div>
+        <div class="Innercontent">Company</div>
+        <div class="Innercontent">Contact</div>
+        <div class="Innercontent">Privacy</div>
+    </div>
+</div>
+</div>`
